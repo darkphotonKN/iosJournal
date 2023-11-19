0101473a5a315bebb78251fb1c9a4ea51189bce8
Kranti <darkphoton20@gmail.com>
2023-11-19 16:23:11 +0800

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Kranti on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
